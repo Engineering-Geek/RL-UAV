b489c812fd1430857e9f7855ddb745a2eaaa65db
nmelgiri <melgirinik@gmail.com>
2024-02-28 19:22:38 -0500

    continued progress

--- testing.py
+++ testing.py
@@ -1,9 +1,7 @@
 from mujoco import MjModel, MjData, viewer, mj_step
 
 
-m = MjModel.from_xml_path('models/simple_scene.xml')
-# m = MjModel.from_xml_path('mujoco_menagerie/skydio_x2/simple_scene.xml')
-# m = MjModel.from_xml_path('simple_scene.xml')
+m = MjModel.from_xml_path('models/scene.xml')
 d = MjData(m)
 
 viewer.launch(m, d)
